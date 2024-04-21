a793ae37ffb0e7fe31ee401997af998eb55f9798
Poutatis <97979895+Poutatis@users.noreply.github.com>
2024-04-22 00:30:39 +0300

    Laboration 1
    Implementerat kod för laboration 1

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,7 +1,31 @@
-public class StringCalculator {
+import java.util.ArrayList;
+import java.util.List;
 
-    public int add(String input) {
-        //TODO implement
-        return 0;
+public class StringCalculator {
+    public int add(String numbers) {
+        if (numbers.isEmpty()) {
+            return 0;
+        } else {
+            String delimiter = ",";
+            if (numbers.startsWith("//")) {
+                int delimiterIndex = numbers.indexOf("\n");
+                delimiter = numbers.substring(2, delimiterIndex);
+                numbers = numbers.substring(delimiterIndex + 1);
+            }
+            String[] nums = numbers.split("[,\n" + delimiter + "]");
+            int sum = 0;
+            List<Integer> negatives = new ArrayList<>();
+            for (String num : nums) {
+                int n = Integer.parseInt(num);
+                if (n < 0) {
+                    negatives.add(n);
+                }
+                sum += n;
+            }
+            if (!negatives.isEmpty()) {
+                throw new IllegalArgumentException("Negatives not allowed: " + negatives);
+            }
+            return sum;
+        }
     }
 }
